241cd1b5fa065854fcd57ed5de75d44a5d95ce29
Isaac Bowes <Isaac.Bowes@hotmail.com>
2022-10-02 11:58:46 +1030

    sounds/lightning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,12 +14,21 @@ public class GameManager : MonoBehaviour
     public float timeRemaining = 0;
     public bool timerIsRunning = false;
     public TextMeshPro timeText;
+    public Light2D lighting;
+    public AudioClip[] lightningCrash;
+    public AudioSource aS;
+    public bool isPlaying;
+
     private void Start()
     {
+        lighting = GetComponent<Light2D>();
+        aS = GetComponent<AudioSource>();
         // Starts the timer automatically
         timerIsRunning = true;
-
+        isPlaying = false;
         Instantiate(enemy);
+
+        
 
     }
     void Awake()
@@ -35,15 +45,17 @@ public class GameManager : MonoBehaviour
     }
     void Update()
     {
+        lightning();
+
         //Timer Count up
         if (timerIsRunning)
         {
-            timeRemaining += Time.deltaTime;
-            DisplayTime(timeRemaining);
+            currentTime += Time.deltaTime;
+            DisplayTime(currentTime);
 
             if (gameOver == true)
             {
-                Debug.Log("Time has run out!");
+                Debug.Log("Time has stopped!");
                 timeRemaining = timeRemaining;
                 timerIsRunning = false;
             }
@@ -54,8 +66,48 @@ public class GameManager : MonoBehaviour
         timeToDisplay += 1;
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        float milliSeconds = (timeToDisplay % 1) * 1000;
-        timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
+        //float milliSeconds = (timeToDisplay % 1) * 1000;
+        //timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    public void lightning()
+    {
+        if (currentTime >= 10f && currentTime < 10.5f)
+        {
+            lighting.intensity = 1;
+            if (!isPlaying)
+                StartCoroutine(playSound());
+        }
+        if(currentTime >= 10.5f && currentTime < 11f)
+        {
+            lighting.intensity = .1f;
+        }
+        if (currentTime >= 11f && currentTime < 11.5f)
+        {
+            lighting.intensity = 1;
+            if(!isPlaying)
+                StartCoroutine(playSound());
+        }
+        if (currentTime >= 11.5f)
+        {
+            lighting.intensity = .1f;
+            currentTime = 0f;
+        }
+    }
+
+
+    IEnumerator playSound()
+    {
+        AudioClip rand = lightningCrash[Random.Range(0, lightningCrash.Length)];
+        isPlaying = true;
+        aS.clip = rand;
+        aS.Play();       //plays big hand audio
+        yield return new WaitForSeconds(aS.clip.length);
+        AudioClip rand2 = lightningCrash[Random.Range(0, lightningCrash.Length)];
+        aS.clip = rand2;
+        aS.Play();       //plays small hand audio after big hand audio
+        yield return new WaitForSeconds(aS.clip.length);
+        isPlaying = false;
+    }
 }
